6bc7ca1cd92f4fc6040250ea00a24232b85f2d37
sajjadislam523 <sajjad.islam523@gmail.com>
2024-10-28 19:35:01 +0600

    updated security key

--- djangomart/settings.py
+++ djangomart/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1c^7+654ebr!bt$0^j(m7q2$+h^pbmj(jch-=_=4nm)ebfw!!8'
+SECRET_KEY = 'django-insecure-ar!(jw!5zd^+s37#jbniss50c+z2u6!1t*#0a2%37cwz3i$@0a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
